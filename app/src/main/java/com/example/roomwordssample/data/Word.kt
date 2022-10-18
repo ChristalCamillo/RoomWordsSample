505cf0bed2f24d7805a727fd3ecf75a3f44650f0
ChristalCamillo <christal.b.camillo@gmail.com>
2022-10-18 18:55:44 -0300

    innitial

---
+++ app/src/main/java/com/example/roomwordssample/data/Word.kt
@@ -0,0 +1,10 @@
+package com.example.roomwordssample.data
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+data class Word(var word: String){
+    @Entity(tableName = "word_table")
+    class Word(@PrimaryKey @ColumnInfo(name = "word") val word: String)
+}
